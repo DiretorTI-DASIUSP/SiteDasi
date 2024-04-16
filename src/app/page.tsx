042facbac7379507fa5b1b9e7c6ca2fca93b63b9
Miguel <75583778+Miguel03706@users.noreply.github.com>
2024-04-16 18:29:20 -0300

    setup do projeto 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import Link from 'next/link'
-
 import React from 'react'
+
+/**
+ Essa é a página inicial do site, no next, a pagina com o nome "page",
+ vai ser iniciada por padrão, sendo assim, ela será a página inicial do site.
+ */
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href='/entrar'>ccc</Link>
+    <div className='container-page'>
+      <header>esse será o header da página</header>
+
+      <main>
+        <h1>Titulo</h1>
+        <p>Olá, bem-vindo ao meu site</p>
+      </main>
+
+      <footer>Esse daqui é o footer</footer>
     </div>
   )
 }
