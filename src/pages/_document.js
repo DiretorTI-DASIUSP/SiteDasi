844cef2b6ae57ac4d48a009fccc7ec431b747cb0
F1NH4WK <eduardo.ar33@gmail.com>
2024-05-24 15:29:14 -0300

    feat: og img width and heigth

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
         <meta property = "og:type" content="website"/>
         <meta property = "og:image" content = "https://github.com/dasiusp/assets/blob/master/arts/letreiro-logo.png?raw=true"/>
         <meta property = "og:description" content = "O grandioso website do DASI! Desenvolvido puramente pelos integrantes do setor de TI do Diretório Acadêmico de Sistemas de Informação"/>      
+        <meta property = "og:image:width" content = "200"/>
+        <meta property="og:image:heigth" content = "100"/>
         </Head>
       <body>
         <Main />
