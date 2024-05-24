c0ff5e52a60784b37ae177b9564afdcdb1142cf7
F1NH4WK <eduardo.ar33@gmail.com>
2024-05-24 14:54:24 -0300

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DASI</title>
+        <meta name="description" content="Website do Diretório Acadêmico de Sistemas de Informação"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+    
+      </main>
+    </>
+  );
+}
