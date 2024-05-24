38e6979ed63b5196afb24ed4331d8d0e2dd4a68a
F1NH4WK <eduardo.ar33@gmail.com>
2024-05-24 15:00:20 -0300

    feat: og meta data

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <meta property = "og:title" content="DASI"/>
+        <meta property = "og:type" content="website"/>
+        <meta property = "og:image" content = "https://github.com/dasiusp/assets/blob/master/arts/letreiro-logo.png?raw=true"/>
+        <meta property = "og:description" content = "O grandioso website do DASI! Desenvolvido puramente pelos integrantes do setor de TI do Diretório Acadêmico de Sistemas de Informação"/>      
+        </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Exemplo from "@/components/Exemplo";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-    
+        <Exemplo/>
       </main>
     </>
   );
