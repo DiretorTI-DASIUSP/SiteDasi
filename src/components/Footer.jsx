d0bfe33e376842c9b319b215159c45f4606040db
F1NH4WK <eduardo.ar33@gmail.com>
2024-07-19 19:29:26 -0300

    fix: Mobile Footer Suggestions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,18 @@ export default function Footer() {
           <hr></hr>
         </Title>{" "}
         {/* Organizei o código em divisões, TextosFooter: que tem os textos e as linhas em baixo dos textos  */}
-        <p>Rua Arlindo Béttio, 1000</p>{" "}
+        <p>Rua Arlindo Béttio, 1000</p>
         {/* Imagens: onde estão as imagens. E por fim MsgDireitos: Que foi excluisavamente para a mensagem no fim da pagina. */}
         <p>São Paulo - SP, 03828-000</p>
         <p>CNPJ - 12.598.380/0001-97</p>
       </Block>
-      {/* Criei a div Block(blocos), para aplicar facilmente o displayflex, para a responsividade, 
-                                                                     separei os títulos pela div Title para além de poupar código, separar o 
-                                                                     título e o hr do paragrafo abaixo de forma simples, de forma que o hr e o h1,h2 se torne "1"  */}
-      <Block>
+
+      {/* Criei a div Block(blocos), para aplicar facilmente o displayflex, para a responsividade,                                                    separei os títulos pela div Title para além de poupar código, separar o 
+      título e o hr do paragrafo abaixo de forma simples, de forma que o hr e o h1,h2 se torne "1"  */}
+     
+     <Block>
         <Title>
-          <h2>Termos</h2>
+          <h1>Termos</h1>
           <hr></hr>
         </Title>
         <p>Código de conduta</p>
@@ -27,9 +28,10 @@ export default function Footer() {
 
       <Block>
         <Title>
-          <h2>Redes Sociais</h2>
-          <hr id="TamanhoLinhaRedes"></hr>
+          <h1>Redes Sociais</h1>
+          <hr></hr>
         </Title>
+
         <Imagens>
           <img src="/images/logotipo-do-instagram.png" />
           <img src="/images/logotipo-do-linkedin.png" />
@@ -42,162 +44,82 @@ export default function Footer() {
     </TextosFooter>
   );
 }
+
 //Aqui na "div" Tittle, vou personalizar o h1,h2 e o hr
 const Title = styled.div`
   h1 {
-    font-size: 35px;
+    font-size: 2rem;
     color: white;
     font-family: "Montserrat", sans-serif;
     font-weight: 700;
-    letter-spacing: 1px;
-  }
-
-  h2 {
-    font-size: 35px;
-    color: white;
-    font-family: "Montserrat", sans-serif;
-    font-weight: 700;
-    letter-spacing: 1px;
   }
 
   hr {
-    width: 100px;
-    height: 3px;
+    width: 20%;
     background-color: white;
   }
+`
 
-  #TamanhoLinhaRedes {
-    width: 90px;
-  }
-`;
 //Aqui na "div" Block, vou separar o código entre essas block's para poder fazer o displayflex facilmente.
 //Também possui o media screen para 768px para centralizar os blocks.
 
 const Block = styled.div`
 
-display: flex; 
-flex-direction:column; 
-margin-left: 30px; 
-gap:30px;
+    display: flex; 
+    flex-direction:column; 
+    
+    gap: 1.5em;
 
-p{
-  
-  font-size: 22px;
-  color: white;
-  font-family: "Montserrat", sans-serif; {/* Fonte momentânea que utilizei, pois a Rubik não estava dadno certo */}
-  font-weight: 300;}
+    p{
+    font-size: 1.4rem;
+    color: white;
+    font-family: "Montserrat", sans-serif; 
+    }
 
-  @media screen and (min-width: 1024px){
-
-margin-left:0px;
-}
-
- @media screen and (min-width: 768px){
+    @media screen and (min-width: 768px){
+      align-items: center;
+    }
 
 
-
- align-items: center;
- margin-left:0px;
-
-
-
-
-`;
+`
 //No TextosFooter eu fiz toda a estilização geral dessa página, mudando a altura nas resoluções etc.
 const TextosFooter = styled.div`
 
-
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-
-
     width: 100%;
-    height: 844px;                                      
+    padding: 4em 2em;                                     
     background-color: #5600C5;
     position:relative;
     display:flex;
     flex-direction:column;
-    gap:90px;
-    padding-top:50px;
-
+    gap: 4em;
 
     @media screen and (min-width: 1024px){
-
-    flex-direction:row;
-    height:334px;
-    justify-content:space-evenly;
-
-    
-  
-
- }
-  
-
-
-
-
-
- 
-    
-}
-
-  
-
-  
-    
-    
-        
-    
-   
-
-    
-
-
-   
-    
-    
+      flex-direction:row;
+      justify-content:space-evenly;      
+    }        
 `;
 
 const Imagens = styled.div`
-  display: flex;
-   {
-    /* Estilos relacionados a imagens, usei displayflex para organizar as 2 imagens. */
-  }
+  display: flex;  
+  /* Estilos relacionados a imagens, usei displayflex para organizar as 2 imagens. */
   flex-direction: row;
-  gap: 40px;
-
+  gap: 2em;
   img {
-    width: 45px;
+    width: 2.5em;
   }
 `;
+
 //Aqui estilizei a mensagem de direitos, organizando seu tamanho e posição nas diferentes resoluções.
 const MsgDireitos = styled.div`
-  position: absolute;
-  bottom: 35px;
-  width: 100%;
 
   p {
     text-align: center;
-     {
-      /* Estilos relacionados a MsgDireitos, para melhor leitura/execução do código */
-    }
-    font-size: 15px;
+     
+    /* Estilos relacionados a MsgDireitos, para melhor leitura/execução do código */
+    
+    font-size: 1rem;
     color: white;
-    line-height: 25px;
     font-family: "Montserrat", sans-serif;
-     {
-      /* Fonte momentânea que utilizei, pois a Rubik não estava dando certo */
-    }
     font-weight: 300;
-  }
-
-  @media screen and (min-width: 768px) {
-    bottom: 4px;
-  }
-
-  @media screen and (min-width: 1024px) {
-    bottom: 10px;
-    p {
-      font-size: 17px;
-    }
   }
 `;
