aed96850236465285eb8af8a816ac3488a758a4a
GustavoElDib <gustavodib0706@gmail.com>
2024-08-29 12:41:22 -0300

    feat: Alterei o SecaoSetores e add CardSetores

--- src/components/CardSetores/CardSetores.jsx
+++ src/components/CardSetores/CardSetores.jsx
@@ -1,13 +1,68 @@
-import React from "react";
-import styles from './CardSetores.module.css'
+import React from 'react';
+import styled from 'styled-components';
 
-export default function CardSetores({foto, titulo, texto}){
-    return (
-    <div className={styles.cardContainer}>
-        <img src={foto} alt={titulo} className={styles.image} />
-        <h3 className={styles.title}>{titulo}</h3>
-        <hr className={styles.divider} />
-        <p className={styles.text}>{texto}</p>
-    </div>
-    );
+export default function CardSetores({ foto, titulo, texto }) {
+  return (
+    <CardsContainer>
+      <Image>
+        <img src={foto} alt={titulo} />
+      </Image>
+      <Title>
+        <h1>{titulo}</h1>
+      </Title>
+      <Divider />
+      <Text>
+        <p>{texto}</p>
+      </Text>
+    </CardsContainer>
+  );
 }
+
+const CardsContainer = styled.section`
+    width: 250px;
+    height: 230px;
+    border-radius: 4px;
+    padding: 20px;
+    display: flex;
+    text-align: center;
+    background: #181818;
+    color: white;
+    margin: 15px;
+    transition: transform 0.3s ease-in-out;
+    flex-direction: column;
+    justify-content: center;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const Image = styled.div`
+  img {
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+`;
+
+const Title = styled.div`
+  h1 {
+    font-size: 20px;
+    font-weight: 600;
+    margin: 10px 0;
+  }
+`;
+
+const Divider = styled.div`
+  margin: 10px 0;
+  border: none;
+  border-top: 1px solid #ffffff30;
+`;
+
+const Text = styled.div`
+  p {
+    font-size: 14px;
+    color: #aaa;
+  }
+`;
--- src/components/SecaoSetores/SecaoSetores.jsx
+++ src/components/SecaoSetores/SecaoSetores.jsx
@@ -1,19 +1,152 @@
 import React from 'react';
-import styles from './SecaoSetores.module.css' // Importar CSS Module
+import styled from 'styled-components';
+import CardSetores from '../CardSetores/CardSetores';
 
-export default function SecaoSetores () {
+export default function SecaoSetores() {
   return (
-    <div className={styles.setoresContainer}>
-    <div className={styles.background}>
-      <div className={styles.containerMainTitle}>
-           <h1 className={styles.mainTitle}>CONHEÇA NOSSOS <span className={styles.highlightSetores}>SETORES</span></h1>   
-      </div>
+    <Container>
+      <Background>
+        <ContainerMainTitle>
+          <MainTitle>
+            <h1>CONHEÇA NOSSOS <span>SETORES</span></h1>
+          </MainTitle>
+        </ContainerMainTitle>
 
-      <div className={styles.containerSubTitle}>
-        <h2 className={styles.subTitle}>No <span className={styles.highlightDasi}>DASI</span> você vai encontrar o setor que mais combina com você!</h2>
-      </div>
-      </div>
-  </div>
-  )
+        <ContainerSubTitle>
+          <SubTitle>
+            <h2>No <span>DASI</span> você vai encontrar o setor que mais combina com você!</h2>
+          </SubTitle>
+        </ContainerSubTitle>
+
+        <CardsWrapper>
+          <CardSetores foto="img_academico" titulo="Acadêmico" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_ti" titulo="TI" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_eventos" titulo="Eventos" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_esportivo" titulo="Esportivo" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_gestao" titulo="Gestão de Pessoas" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_administração" titulo="Administração" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          <CardSetores foto="img_patrimonio" titulo="Patrimônio" texto="Lorem ipsum is simply dummy text of the printing and typesetti..." />
+          
+        </CardsWrapper>
+      </Background>
+    </Container>
+  );
 }
 
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: rgb(86, 0, 197);
+  background-size: cover;
+  color: white;
+  text-align: center;
+  padding: 20px;
+  align-items: stretch;
+`;
+
+const Background = styled.div`
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MainTitle = styled.div`
+  h1 {
+    font-family: Montserrat;
+    font-size: 3.5rem;
+    font-weight: 300;
+    line-height: 78.02px;
+    text-align: center;
+    filter: drop-shadow(0px 4px 0px #00000040);
+  }
+
+  span {
+    font-family: Montserrat;
+    font-size: 3.5rem;
+    font-weight: 600;
+    line-height: 78.02px;
+    text-align: center;
+  }
+
+  @media (max-width: 1200px) {
+    h1, span {
+      font-size: 2.5rem;
+    }
+  }
+
+  @media (max-width: 992px) {
+    h1, span {
+      font-size: 2rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h1, span {
+      font-size: 1.75rem;
+    }
+  }
+
+  @media (max-width: 576px) {
+    h1, span {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+const SubTitle = styled.div`
+  h2 {
+    font-family: 'Montserrat';
+    font-size: 1.8rem;
+    font-weight: 100;
+    line-height: 43.88px;
+    text-align: center;
+    filter: drop-shadow(0px 4px 0px #00000040);
+  }
+
+  span {
+    font-family: Montserrat;
+    font-size: 27px;
+    font-weight: 600;
+    line-height: 43.88px;
+    text-align: center;
+  }
+
+  @media (max-width: 1200px) {
+    h2, span {
+      font-size: 1.5rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h2, span {
+      font-size: 1.156rem;;
+    }
+  }
+
+  @media (max-width: 576px) {
+    h2, span {
+      font-size: 1.2rem;
+    }
+  }
+`;
+
+const ContainerMainTitle = styled.div``;
+
+const ContainerSubTitle = styled.div`
+  width: 100%;
+  height: 88px;
+  max-width: 580px;
+`;
+
+const CardsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 40px;
+  width: 100%;
+  max-width: 70rem;
+  filter: drop-shadow(0px 2px 1px #FFFFFF);
+`;
